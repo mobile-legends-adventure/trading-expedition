d30305235eec32d90e43e515ed2e0644f14baf9a
mobile-legends-adventure <rockiedoc.69@gmail.com>
2024-07-29 19:09:33 +0300

    ps 1.1

--- src/layouts/main/MainLayout.tsx
+++ src/layouts/main/MainLayout.tsx
@@ -4,7 +4,6 @@ import MainPage from '../../pages/main/MainPage';
 const MainLayout: FC = () => {
     return (
         <div>
-            Main layout
             <MainPage />
         </div>
     );
